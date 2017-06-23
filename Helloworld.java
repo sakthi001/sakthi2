c06a69e545f9763a61a3a62dbc47e5db7f713f96
sakthi001 <sakthivelpavi1997@gmail.com>
2017-06-23 16:03:43 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,12 @@
+package guvi;
+
+	import java.util.Scanner;
+	public class HelloWorld{
+
+	     public static void main(String []args){
+	         int i;
+	        for(i=0;i<5;i++)
+	            System.out.println("hello");
+	     }
+	}
+
