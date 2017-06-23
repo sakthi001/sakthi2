5b9d13609f36cc016befe60de7444565a1b10c9e
sakthi001 <sakthivelpavi1997@gmail.com>
2017-06-23 12:05:26 +0530

    Add files via upload

---
+++ odd.java.java
@@ -0,0 +1,17 @@
+package guvi;
+ import java.util.*;
+
+public class odd {
+ 	 
+	   public static void main(String[] args)
+	   {
+	     Scanner num=new Scanner(System.in);
+	     int n=num.nextInt();
+	     if(n%2==0)
+	       System.out.println("Even");
+	     else
+	  		System.out.println("Odd");
+	   }
+
+
+}
