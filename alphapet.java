8818f92760fd73c3d71d9a90048cf17e10f94133
sakthi001 <sakthivelpavi1997@gmail.com>
2017-06-23 14:51:27 +0530

    Add files via upload

---
+++ alphapet.java
@@ -0,0 +1,25 @@
+package guvi;
+	import java.util.Scanner;
+
+	public class alphapet
+	{
+	    public static void main(String args[])
+	    {
+	        char ch;
+	        Scanner scan = new Scanner(System.in);
+			
+	        System.out.print("Enter a Character : ");
+	        ch = scan.next().charAt(0);
+			
+	        if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
+	        {
+	            System.out.print(ch + " is an Alphabet");
+	        }
+	        else
+	        {
+	            System.out.print(ch + " is not an Alphabet");
+	        }
+	    }
+	}
+
+
