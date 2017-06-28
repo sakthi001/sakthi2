11604f3b75e9ffea42b4d404b21b0d3ef1e69537
sakthi001 <sakthivelpavi1997@gmail.com>
2017-06-28 10:57:53 +0530

    Rename StringtoInt.java to StringInt.java

---
+++ StringInt.java
@@ -0,0 +1,12 @@
+import java.io.*;
+	class StringToInt{
+	public static void main(String args[]){
+	int intoInt;
+	String s;
+	Scanner sc=new Scanner(System.in);
+	s=sc.next();
+	intoInt=Integer.parseInt(s);
+	System.out.println("This is an integer: "+intoInt);
+	}
+	}
+
