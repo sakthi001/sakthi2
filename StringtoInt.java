ed6995a1eb66038a12bfa2f2fc820c127f429b9f
sakthi001 <sakthivelpavi1997@gmail.com>
2017-06-28 10:18:05 +0530

    Add files via upload

--- StringtoInt.java
+++ StringtoInt.java
@@ -1,5 +1,15 @@
 package guvi;
 
-public class StringtoInt {
 
-}
+	import java.io.*;
+	class StringToInt{
+	public static void main(String args[]){
+	int intoInt;
+	String s;
+	Scanner scan=new Scanner(System.in);
+	s=scan.next();
+	intoInt=Integer.parseInt(s);
+	System.out.println("This is an integer: "+intoInt);
+	}
+	}
+
